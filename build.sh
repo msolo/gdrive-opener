472507b0f9ca260acc38d3aab81ad68205a0d90a
Mike Solomon <msolo@gmail.com>
2025-03-21 09:49:03 -0700

    Fix gsheet handling.

--- build.sh
+++ build.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
+# Make sure the Platypus app has installed it's CLI
+
 /usr/local/bin/platypus \
   --droppable \
   --quit-after-execution \
   --app-icon '/Applications/Platypus.app/Contents/Resources/PlatypusDefault.icns' \
   --name 'GDrive Opener' \
+  --app-version '1.1' \
   --interface-type 'None' \
   --interpreter '/usr/bin/python3' \
   --author 'msolo' \
--- gdrive-opener.py
+++ gdrive-opener.py
@@ -3,18 +3,34 @@
 import json
 import os
 import shlex
+import subprocess
 import sys
 
+domain = "com.github.msolo.GDriveOpener"
 docs_url = "https://docs.google.com/document/d/"
+sheets_url = "https://docs.google.com/spreadsheets/d/"
 
+def get_ns_default_string(key='', default=None):
+    try:
+        return subprocess.check_output(['/usr/bin/defaults', 'read', domain, key]).decode().strip()
+    except:
+        return default
+
+
+default_command = '''open -a "Google Chrome"'''
 
 def main():
     for f in sys.argv[1:]:
         d = json.loads(open(f).read())
         url = d.get("url")
         if not url:
-            url = docs_url + d["doc_id"]
-        cmd = shlex.join(["open", "-a", "Google Chrome", url])
+            if f.endswith(".gsheet"):
+                url = sheets_url + d["doc_id"]
+            else:
+                url = docs_url + d["doc_id"]
+
+        exec_cmd = shlex.split(get_ns_default_string("ExecCommand", default_command))
+        cmd = shlex.join(exec_cmd + [url])
         os.system(cmd)
 
 
